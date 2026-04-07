187c1a7ce2fce58c78a13c9a8732a33fc3646bc8
Kanlux <128984626+Kanlux@users.noreply.github.com>
2026-04-07 19:11:13 +0300

    Change email domain

--- src/main/java/DataUser.java
+++ src/main/java/DataUser.java
@@ -1,5 +1,5 @@
 public class DataUser {
-    public static String email = "s.bordiyan@nv.dunice.net";
+    public static String email = "s.bordiyan@yandex.ru";
     public static String password = "He252013";
     public static String login = "Serjik";
     public static String invalidemail = "sergey.bordiyanyandex.ru";
